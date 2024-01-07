3602a74751d775b715e31e275e6be1d2bda660b0
tonkatsuki <6411441+tonkatsuki@users.noreply.github.com>
2024-01-07 13:11:58 -0600

    Final

--- get_vars.py
+++ get_vars.py
@@ -1,12 +1,24 @@
 from datetime import datetime
+import re
+
+
+def sanitize_name(name):
+    sanitized = re.sub(r"(--.*$)|(')|(;)", "", name, flags=re.MULTILINE)
+    sanitized = re.sub(r"[^a-zA-Z0-9\s]", "", sanitized)
+    return sanitized
+
+
+def sanitize_reason(reason):
+    sanitized = re.sub(r"(--.*$)|(')|(;)", "", reason, flags=re.MULTILINE)
+    return sanitized
 
 
 def get_vars(ban):
     authid = ban["authid"]
-    name = ban["name"]
+    name = sanitize_name(ban["name"])
     created = datetime.utcfromtimestamp(ban["created"])
     ends = datetime.utcfromtimestamp(ban["ends"])
     length = ban["length"]
-    reason = ban["reason"]
+    reason = sanitize_reason(ban["reason"])
 
     return authid, name, created, ends, length, reason
--- sqlscript_make.py
+++ sqlscript_make.py
@@ -1,5 +1,5 @@
 def sqlscript_make(sqlscriptfile, commid, name, created, ends, length, reason):
-    queryInsert = f"INSERT INTO `sa_bans` (`player_steamid`, `player_name`, `player_ip`, `admin_steamid`, `admin_name`, `reason`, `duration`, `ends`, `created`) \nVALUES ('{commid}', '{name}', 'NULL', 'Console', 'Console', '{reason}', {length}, timestamp('{ends}'), timestamp('{created}'));"
+    queryInsert = f"INSERT INTO `sa_bans` (`player_steamid`, `player_name`, `player_ip`, `admin_steamid`, `admin_name`, `reason`, `duration`, `ends`, `created`) \nVALUES ('{commid}', '{name}', 'NULL', 'Console', 'Console', '{reason}', {length}, timestamp('{ends}'), timestamp('{created}'));\n"
 
     with open(sqlscriptfile, "a") as file:
         file.write(queryInsert)
